5e2a1604af2631ceef610b44cf5252d218dbab19
jsemalvarez <jsemalvarez@gmail.com>
2023-02-22 17:07:48 -0300

    header circular

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -11,3 +11,21 @@ class HeaderCudrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+        color:Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        )
+      ),
+    );
+  }
+}
