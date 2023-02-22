10a1970e305de947b590615277f10eed86731fbd
jsemalvarez <jsemalvarez@gmail.com>
2023-02-22 17:04:34 -0300

    header cuadrado

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderCudrado extends StatelessWidget {
+  const HeaderCudrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color:const Color(0xff615AAB),
+    );
+  }
+}
