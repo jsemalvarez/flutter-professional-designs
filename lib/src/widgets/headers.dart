1090b66c7e67650ad85bb64d8caf8b7e7a32adf9
jsemalvarez <jsemalvarez@gmail.com>
2023-02-23 03:58:25 -0300

    header pico

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -125,3 +125,51 @@ class _HeaderTrinagularPainter extends CustomPainter {
   @override
   bool shouldRebuildSemantics(_HeaderTrinagularPainter oldDelegate) => false;
 }
+
+class HeaderPico extends StatelessWidget {
+  const HeaderPico({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color:const Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderPicoPainter()
+      ),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final paint = Paint();
+
+    //propiedades
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+
+    //Dibujar con el lapiz y le path
+    final path = Path();
+
+    // path.lineTo(0, size.height);
+    path.lineTo(0, size.height * 0.3);
+    path.lineTo(size.width * 0.5, size.height * 0.33);
+    path.lineTo(size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(_HeaderPicoPainter oldDelegate) => true;
+
+  @override
+  bool shouldRebuildSemantics(_HeaderPicoPainter oldDelegate) => false;
+}
