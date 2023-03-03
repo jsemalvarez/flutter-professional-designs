236598dafb9952e1911d2efec399c8318630b111
jsemalvarez <jsemalvarez@gmail.com>
2023-03-02 21:52:57 -0300

    creando un slideshow

--- lib/src/pages/slides_show_page.dart
+++ lib/src/pages/slides_show_page.dart
@@ -2,16 +2,53 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class SlidesShowPage extends StatelessWidget {
   const SlidesShowPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/slide-4.svg'),
+        child: _Slides(),
       ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  const _Slides({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: const EdgeInsets.all(10),
+      child: PageView( 
+        children: const [
+          _Slide(svg:'assets/svgs/slide-1.svg'),
+          _Slide(svg:'assets/svgs/slide-2.svg'),
+          _Slide(svg:'assets/svgs/slide-3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String svg;
+
+  const _Slide({
+    Key? key, 
+    required this.svg,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg)
     );
   }
 }
